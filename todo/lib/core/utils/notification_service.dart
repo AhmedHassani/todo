766f80adc7f0a4d42d97a8a80044c9dd8f9bc347
Ahmed Rhman Hassani <ah.jr22@gmail.com>
2024-04-27 20:38:27 +0300

    Update notification_service.dart

--- todo/lib/core/utils/notification_service.dart
+++ todo/lib/core/utils/notification_service.dart
@@ -6,29 +6,19 @@ import 'package:timezone/data/latest.dart' as tz;
 import 'package:intl/intl.dart';
 
 class NotificationService {
-
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
   FlutterLocalNotificationsPlugin();
-
   Random random = Random();
-
   Future<void> showNotification(String date,String title,String body) async {
-
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
-    AndroidNotificationDetails(
-      "TODO232",
-      "TODO",
-      importance: Importance.max,
-      priority: Priority.high,
-    );
+    AndroidNotificationDetails( "TODO232", "TODO", importance: Importance.max, priority: Priority.high, );
     DateFormat inputFormat = DateFormat('yyyy-M-dd HH:mm');
     DateTime dateTime = inputFormat.parse(date);
     const NotificationDetails platformChannelSpecifics =
     NotificationDetails(android: androidPlatformChannelSpecifics);
     await flutterLocalNotificationsPlugin.zonedSchedule(
       random.nextInt(100000) + 1, // notification id
-      title,
-      body,
+      title, body,
       tz.TZDateTime.from(dateTime, tz.local),// scheduled date and time
       platformChannelSpecifics,
       androidAllowWhileIdle: true,
